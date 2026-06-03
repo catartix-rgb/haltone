df55eaf2ee66643d4a9a19908582a2ade5b6a0b1
catartix-rgb <catartix@gmail.com>
2026-06-03 16:23:01 -0600

    Delete src directory

--- src/hooks/use-active-layer.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useAppStore } from '@/hooks/use-app-store'
-import type { Layer } from '@/types'
-
-/** Reactively returns the currently-selected layer. */
-export function useActiveLayer(): Layer {
-  return useAppStore((s) => s.doc.layers.find((l) => l.id === s.doc.selectedLayerId) ?? s.doc.layers[0])
-}
